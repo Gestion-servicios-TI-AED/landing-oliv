4613a87311abc8fb6abb33751760b3d4b13e8eb1
Gabriel Elías Valdelamar Caldera <gvaldelamar2003@outlook.com>
2026-05-21 13:33:36 -0500

    chore: rename route to /politica-de-tratamiento-de-datos

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import "./styles/index.css";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navigate to="/politica" replace />} />
-      <Route path="/politica" element={<App />} />
+      <Route path="/" element={<Navigate to="/politica-de-tratamiento-de-datos" replace />} />
+      <Route path="/politica-de-tratamiento-de-datos" element={<App />} />
     </Routes>
   </BrowserRouter>
 );
