63bfa0d0ce05b6e9a88118f4e38382116fc26448
Gabriel Elías Valdelamar Caldera <gvaldelamar2003@outlook.com>
2026-05-27 08:14:23 -0500

    Arreglos

--- src/app/pages/BrochureDigital.tsx
+++ src/app/pages/BrochureDigital.tsx
@@ -1,15 +1,9 @@
+import { useEffect } from "react";
+
 export default function BrochureDigital() {
-  return (
-    <iframe
-      src="/brochure-digital.html"
-      title="Brochure Digital OLIV"
-      style={{
-        position: "fixed",
-        inset: 0,
-        width: "100%",
-        height: "100%",
-        border: "none",
-      }}
-    />
-  );
+  useEffect(() => {
+    window.location.replace("/brochure-digital.html");
+  }, []);
+
+  return null;
 }
